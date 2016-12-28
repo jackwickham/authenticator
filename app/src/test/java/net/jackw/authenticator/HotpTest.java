c07596178fa208d5e8bcde7e3208ae9093f05dca
Jack Wickham <jackwickham@live.co.uk>
2016-12-28 14:15:14 +0000

    Added basic HOTP tests

--- app/src/test/java/net/jackw/authenticator/HotpTest.java
+++ app/src/test/java/net/jackw/authenticator/HotpTest.java
@@ -12,8 +12,15 @@ import static org.junit.Assert.*;
 public class HotpTest {
 	@Test
 	public void code_isCorrect() throws Exception {
-		byte[] secret = {};
+		byte[] secret = { (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78, (byte) 0x90, (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78, (byte) 0x90 };
         HotpGenerator generator = new CounterHotp(secret, HotpGenerator.HashAlgorithm.sha1, 6, 1);
-		assertEquals("", generator.generateCode());
+		assertEquals("803282", generator.generateCode());
     }
+
+	@Test
+	public void code_leadingZero () throws Exception {
+		byte[] secret = { (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78, (byte) 0x90, (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78, (byte) 0x90 };
+		HotpGenerator generator = new CounterHotp(secret, HotpGenerator.HashAlgorithm.sha1, 6, 2);
+		assertEquals("039425", generator.generateCode());
+	}
 }
