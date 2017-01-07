1374c24a4241195bea24985744bee88b4385146f
Jack Wickham <jackwickham@live.co.uk>
2017-01-07 20:43:30 +0000

    Reformatted and rearranged code

--- app/src/main/java/net/jackw/authenticator/MainActivity.java
+++ app/src/main/java/net/jackw/authenticator/MainActivity.java
@@ -16,140 +16,139 @@ import android.widget.*;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-    public static int REFRESH_INTERVAL = 100; // ms
+	public static int REFRESH_INTERVAL = 100; // ms
 
-    private List<Account> accounts;
-    private RefreshHandler refreshTask = null;
-    private AccountListAdapter listAdapter = null;
+	private List<Account> accounts;
+	private RefreshHandler refreshTask = null;
+	private AccountListAdapter listAdapter = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        // Init db
-        DatabaseHelper dbHelper = DatabaseHelper.init(getApplicationContext());
+		// Init db
+		DatabaseHelper dbHelper = DatabaseHelper.init(getApplicationContext());
 
-        accounts = dbHelper.getAccountsDb().getAllAccounts();
+		accounts = dbHelper.getAccountsDb().getAllAccounts();
 
-        ListView accountList = (ListView) findViewById(R.id.account_list);
-        listAdapter = new AccountListAdapter(this, R.layout.account_row, accounts);
-        accountList.setAdapter(listAdapter);
+		ListView accountList = (ListView) findViewById(R.id.account_list);
+		listAdapter = new AccountListAdapter(this, R.layout.account_row, accounts);
+		accountList.setAdapter(listAdapter);
 
-        refreshTask = new RefreshHandler();
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch(id) {
-            case R.id.action_settings:
-                return true;
-            case R.id.action_add:
-                addAccount();
-                return true;
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void addAccount () {
-        Intent intent = new Intent(this, AddActivity.class);
-
-        startActivity(intent);
-    }
+		refreshTask = new RefreshHandler();
+	}
 
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 
-    private class AccountListAdapter extends ArrayAdapter<Account> {
-        public AccountListAdapter(Context context, int userRowId, List<Account> accounts) {
-            super(context, userRowId, accounts);
-        }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent){
-            LayoutInflater inflater = getLayoutInflater();
-            Account account = getItem(position);
+		switch (id) {
+			case R.id.action_settings:
+				return true;
+			case R.id.action_add:
+				addAccount();
+				return true;
 
-            View row;
-            if (convertView != null) {
-                // Reuse the existing view
-                row = convertView;
-            } else {
-                // Create new one
-                row = inflater.inflate(R.layout.account_row, null);
-            }
+		}
 
-            TextView codeView = (TextView) row.findViewById(R.id.code);
-            TextView labelView = (TextView) row.findViewById(R.id.code_label);
-            CountdownIndicator countdown = (CountdownIndicator) row.findViewById(R.id.countdown_indicator);
+		return super.onOptionsItemSelected(item);
+	}
 
-            String code = account.getCodeGenerator().getCodeForDisplay();
+	public void addAccount() {
+		Intent intent = new Intent(this, AddActivity.class);
 
-            CodeGenerator.Type type = account.getCodeGenerator().getType();
-            if (type == CodeGenerator.Type.TOTP) {
-                float timeRemaining = ((Totp) account.getCodeGenerator()).getTimeRemainingFraction();
+		startActivity(intent);
+	}
 
-                countdown.setVisibility(View.VISIBLE);
-                countdown.setPhase(timeRemaining);
-            } else if (type == CodeGenerator.Type.HOTP) {
-                countdown.setVisibility(View.GONE);
-            }
 
-            ImageView imageView = (ImageView) row.findViewById(R.id.account_image);
-            if (account.hasImage()) {
-                imageView.setVisibility(View.VISIBLE);
-                imageView.setImageBitmap(account.getImage());
-            } else {
-                imageView.setVisibility(View.GONE);
-            }
+	private class AccountListAdapter extends ArrayAdapter<Account> {
+		public AccountListAdapter(Context context, int userRowId, List<Account> accounts) {
+			super(context, userRowId, accounts);
+		}
 
-            // Set the code
-            codeView.setText(code);
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = getLayoutInflater();
+			Account account = getItem(position);
 
-            // Set the label
-            String label = account.getIssuer() + " (" + account.getUsername() + ")";
-            Spannable labelSpan = new SpannableString(label);
-            labelSpan.setSpan(new ForegroundColorSpan(ContextCompat.getColor(MainActivity.this, R.color.label_username)), account.getIssuer().length(), label.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+			View row;
+			if (convertView != null) {
+				// Reuse the existing view
+				row = convertView;
+			} else {
+				// Create new one
+				row = inflater.inflate(R.layout.account_row, null);
+			}
 
-            labelView.setText(labelSpan, TextView.BufferType.SPANNABLE);
+			TextView codeView = (TextView) row.findViewById(R.id.code);
+			TextView labelView = (TextView) row.findViewById(R.id.code_label);
+			CountdownIndicator countdown = (CountdownIndicator) row.findViewById(R.id.countdown_indicator);
 
-            return row;
-        }
-    }
+			String code = account.getCodeGenerator().getCodeForDisplay();
 
-    private class RefreshHandler {
-        private final Handler h;
+			CodeGenerator.Type type = account.getCodeGenerator().getType();
+			if (type == CodeGenerator.Type.TOTP) {
+				float timeRemaining = ((Totp) account.getCodeGenerator()).getTimeRemainingFraction();
 
-        public RefreshHandler () {
-            h = new Handler();
-            h.postDelayed(new RefreshRunner(), REFRESH_INTERVAL);
-        }
+				countdown.setVisibility(View.VISIBLE);
+				countdown.setPhase(timeRemaining);
+			} else if (type == CodeGenerator.Type.HOTP) {
+				countdown.setVisibility(View.GONE);
+			}
 
-        private class RefreshRunner implements Runnable {
-            @Override
-            public void run () {
-                listAdapter.notifyDataSetChanged();
+			ImageView imageView = (ImageView) row.findViewById(R.id.account_image);
+			if (account.hasImage()) {
+				imageView.setVisibility(View.VISIBLE);
+				imageView.setImageBitmap(account.getImage());
+			} else {
+				imageView.setVisibility(View.GONE);
+			}
 
-                h.postDelayed(this, REFRESH_INTERVAL);
-            }
-        }
+			// Set the code
+			codeView.setText(code);
 
-    }
+			// Set the label
+			String label = account.getIssuer() + " (" + account.getUsername() + ")";
+			Spannable labelSpan = new SpannableString(label);
+			labelSpan.setSpan(new ForegroundColorSpan(ContextCompat.getColor(MainActivity.this, R.color.label_username)), account.getIssuer().length(), label.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+			labelView.setText(labelSpan, TextView.BufferType.SPANNABLE);
+
+			return row;
+		}
+	}
+
+	private class RefreshHandler {
+		private final Handler h;
+
+		public RefreshHandler() {
+			h = new Handler();
+			h.postDelayed(new RefreshRunner(), REFRESH_INTERVAL);
+		}
+
+		private class RefreshRunner implements Runnable {
+			@Override
+			public void run() {
+				listAdapter.notifyDataSetChanged();
+
+				h.postDelayed(this, REFRESH_INTERVAL);
+			}
+		}
+
+
+	}
 }
